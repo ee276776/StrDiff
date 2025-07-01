c46de08d8b7d2e90da2e295b3de863c916aa3e92
ianchen <ee2767762gmail.com>
2025-07-02 00:32:40 +0800

    init push

---
+++ 比較缺少/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(50, 77)
+        Me.TextBox1.MaxLength = 0
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(226, 509)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.WordWrap = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(566, 75)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(79, 35)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "分析"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(186, 48)
+        Me.TextBox2.MaxLength = 0
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(32, 27)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Visible = False
+        Me.TextBox2.WordWrap = False
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(320, 77)
+        Me.TextBox3.MaxLength = 0
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox3.Size = New System.Drawing.Size(226, 509)
+        Me.TextBox3.TabIndex = 3
+        Me.TextBox3.WordWrap = False
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(439, 44)
+        Me.TextBox4.MaxLength = 0
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox4.Size = New System.Drawing.Size(32, 27)
+        Me.TextBox4.TabIndex = 4
+        Me.TextBox4.Visible = False
+        Me.TextBox4.WordWrap = False
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(927, 75)
+        Me.TextBox5.MaxLength = 0
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox5.Size = New System.Drawing.Size(226, 509)
+        Me.TextBox5.TabIndex = 5
+        Me.TextBox5.WordWrap = False
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(673, 75)
+        Me.TextBox6.MaxLength = 0
+        Me.TextBox6.Multiline = True
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox6.Size = New System.Drawing.Size(226, 509)
+        Me.TextBox6.TabIndex = 6
+        Me.TextBox6.WordWrap = False
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(1180, 77)
+        Me.TextBox7.MaxLength = 0
+        Me.TextBox7.Multiline = True
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox7.Size = New System.Drawing.Size(226, 509)
+        Me.TextBox7.TabIndex = 7
+        Me.TextBox7.WordWrap = False
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(52, 51)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox8.TabIndex = 9
+        Me.TextBox8.Text = "A"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(320, 51)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox9.TabIndex = 10
+        Me.TextBox9.Text = "B"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(671, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 12)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "A有B沒有"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(925, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 12)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "B有A沒有"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(1178, 51)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 12)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "A,B共有"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1444, 658)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "比對內容"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ 比較缺少/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+    Dim member As New Dictionary(Of String, String)
+    Dim member2 As New Dictionary(Of String, String)
+    Private Structure profile
+        Dim name As String
+
+    End Structure
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        member.Clear()
+        member2.Clear()
+        TextBox5.Clear()
+        TextBox6.Clear()
+        TextBox7.Clear()
+        TextBox2.Text = String.Join(Environment.NewLine, TextBox1.Lines.Distinct())
+        Dim a As Array = TextBox2.Lines
+        Array.Sort(a)
+        TextBox2.Lines = a
+
+        TextBox4.Text = String.Join(Environment.NewLine, TextBox3.Lines.Distinct())
+        Dim b As Array = TextBox4.Lines
+        Array.Sort(b)
+        TextBox4.Lines = b
+
+
+
+        For i = 0 To TextBox2.Lines.Count - 1
+            Dim x As String
+            Dim Id = i
+            x = Trim(TextBox2.Lines(i))
+            member.Add(Id, x)
+        Next
+        For i = 0 To TextBox4.Lines.Count - 1
+            Dim x As String
+            Dim Id = i
+            x = Trim(TextBox4.Lines(i))
+            member2.Add(Id, x)
+        Next
+        For Each x In member2
+            If member.ContainsValue(x.Value) Then
+                TextBox7.AppendText(x.Value & vbCrLf)
+            Else
+                TextBox5.AppendText(x.Value & vbCrLf)
+            End If
+        Next
+        For Each x In member
+            If member2.ContainsValue(x.Value) Then
+                TextBox7.AppendText(x.Value & vbCrLf)
+            Else
+                TextBox6.AppendText(x.Value & vbCrLf)
+            End If
+        Next
+        TextBox7.Text = String.Join(Environment.NewLine, TextBox7.Lines.Distinct())
+
+    End Sub
+
+    Private Sub TextBox8_TextChanged(sender As Object, e As EventArgs) Handles TextBox8.TextChanged
+        Label1.Text = TextBox8.Text & "有" & TextBox9.Text & "沒有"
+        Label2.Text = TextBox9.Text & "有" & TextBox8.Text & "沒有"
+        Label3.Text = TextBox8.Text & "," & TextBox9.Text & "共有"
+    End Sub
+
+    Private Sub TextBox9_TextChanged(sender As Object, e As EventArgs) Handles TextBox9.TextChanged
+        Label1.Text = TextBox8.Text & "有" & TextBox9.Text & "沒有"
+        Label2.Text = TextBox9.Text & "有" & TextBox8.Text & "沒有"
+        Label3.Text = TextBox8.Text & "," & TextBox9.Text & "共有"
+    End Sub
+    'Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    '    ListBox1.Items.Clear()
+    '    For Each x In member
+    '        ListBox1.Items.Add(x.Key)
+    '    Next
+    'End Sub
+    'Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+    '    Dim x As profile = member.Item(ListBox1.Text)
+    '    Debug.Print(ListBox1.Text)
+    '    TextBox1.Text = x.birthday
+    '    TextBox2.Text = x.name
+    '    TextBox3.Text = ListBox1.Text
+    '    RadioButton1.Checked = x.gender
+    '    RadioButton2.Checked = Not x.gender
+    'End Sub
+End Class
